5e923888ad34ad3f9428dbe2d043c68701246199
ECNUpersister <2843903421@qq.com>
2022-03-15 19:03:38 +0800

    impr

--- practice/dataset.py
+++ practice/dataset.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import torch
 from PIL import Image
@@ -11,7 +10,6 @@ class MyDataset(torch.utils.data.Dataset):
         self.transforms = transforms
 
     def __getitem__(self, idx):
-        # load images and masks
         if self.image_path_list[idx].endswith('npy'):
             img = np.load(self.image_path_list[idx])
             img = Image.fromarray(img)
@@ -20,35 +18,12 @@ class MyDataset(torch.utils.data.Dataset):
             img = Image.open(self.image_path_list[idx])
             mask = Image.open(self.mask_path_list[idx])
             mask = np.array(mask)
-        # img = img.convert("RGB")
-        # instances are encoded as different colors
-        obj_ids = np.unique(mask)
-        # first id is the background, so remove it
-        obj_ids = obj_ids[1:]
-
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = mask == obj_ids[:, None, None]
-
-        # get bounding box coordinates for each mask
-        boxes = []
         pos = np.where(mask)
-        xmin = np.min(pos[1])
-        xmax = np.max(pos[1])
-        ymin = np.min(pos[0])
-        ymax = np.max(pos[0])
-        boxes.append([xmin, ymin, xmax, ymax])
-
-        # convert everything into a torch.Tensor
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
+        boxes = torch.as_tensor([[np.min(pos[1]), np.min(pos[0]), np.max(pos[1]), np.max(pos[0])]], dtype=torch.float32)
         labels = torch.ones((1,), dtype=torch.int64)
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
-
+        masks = torch.as_tensor(mask[np.newaxis, :, :], dtype=torch.uint8)
         target = {"boxes": boxes, "labels": labels, "masks": masks}
-
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+        img, target = self.transforms(img, target)
 
         return img, target
 
